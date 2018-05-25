3a9ce96aae17806158f81d9f180e62aaf6dc952c
Apple <Apple@Apples-MBP-47.lan>
2018-05-24 23:28:15 -0500

    Currency Converter
    A currency converter app that converts dollars to pounds.

---
+++ CurrencyConverter/app/src/main/java/com/rakipbebo/currencyconverter/MainActivity.java
@@ -0,0 +1,31 @@
+package com.rakipbebo.currencyconverter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convert(View view)
+    {
+        EditText dollarAmountEditText = (EditText) findViewById(R.id.dollarEditText);
+
+        Double dollarAmountDouble = Double.parseDouble(dollarAmountEditText.getText().toString());
+
+        Double poundAmount = dollarAmountDouble * 0.75;
+
+        Toast.makeText(MainActivity.this,"In Pounds " + String.format("%.2f",poundAmount),Toast.LENGTH_SHORT).show();
+
+
+        Log.i("amount", dollarAmountEditText.getText().toString());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
